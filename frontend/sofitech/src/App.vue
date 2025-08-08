180c7807f453c1ab78e16e82763277293dd0cd83
john <jhonandres4360435@gmail.com>
2025-08-08 15:29:00 -0500

    agregar el navbar

--- frontend/sofitech/src/App.vue
+++ frontend/sofitech/src/App.vue
@@ -1,11 +1,10 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+//componentes
+import Navbar from './components/Navbar.vue'
+</script>
 
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+<Navbar/>
 </template>
 
 <style scoped></style>
---
+++ frontend/sofitech/src/components/Navbar.vue
@@ -0,0 +1,33 @@
+<template>
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <div class="container-fluid">
+    <a class="nav-link" href="#">Sisanar</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link" aria-current="page" href="#">Cuidarte en casa</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Medica Colombia</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Clinica San Fernando</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link  " href="#">Factoring S.A.S</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link  " href="#">Transervisocial</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+</template>
+<script setup lang="ts">
+</script>
+<style scoped>
+</style>
